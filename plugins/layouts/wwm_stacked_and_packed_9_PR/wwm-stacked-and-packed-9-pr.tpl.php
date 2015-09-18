feaa24eec8746db27bd28d0d7640ab6437eca1bb
palagpatski <palagpatski@gmail.com>
2015-09-18 17:00:06 +0800

    Added classes to internal content regions.

--- plugins/layouts/wwm_stacked_and_packed_9_PR/wwm-stacked-and-packed-9-pr.tpl.php
+++ plugins/layouts/wwm_stacked_and_packed_9_PR/wwm-stacked-and-packed-9-pr.tpl.php
@@ -64,11 +64,11 @@
       </div>
     <?php endif; ?>
   	<div class="content-main">
-  	  <?php print $content['content_inner_header']; ?>
-      <?php print $content['content_inner_top_left']; ?>
-      <?php print $content['content_inner_top_right']; ?>
-      <?php print $content['content_inner_main']; ?>
-      <?php print $content['content_inner_footer']; ?>
+  	  <div class="content-inner-header"><?php print $content['content_inner_header']; ?></div> 
+      <div class="content-inner-top-left"><?php print $content['content_inner_top_left']; ?></div>
+      <div class="content-inner-top-right"><?php print $content['content_inner_top_right']; ?></div>
+      <div class="content-inner-main"><?php print $content['content_inner_main']; ?></div>
+      <div class="content-inner-footer"><?php print $content['content_inner_footer']; ?></div>
   	</div>
     <?php if ($content['content_bottom']): ?>
     	<div class="content-bottom">
