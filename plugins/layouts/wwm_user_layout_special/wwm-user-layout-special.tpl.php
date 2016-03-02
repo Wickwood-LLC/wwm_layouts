8815b02e0e5e2a625e96204e519ef7ea556abe65
palagpatski <palagpatski@gmail.com>
2016-03-02 17:23:18 +0800

    WWM User Layout Special v2

--- plugins/layouts/wwm_user_layout_special/wwm-user-layout-special.tpl.php
+++ plugins/layouts/wwm_user_layout_special/wwm-user-layout-special.tpl.php
@@ -9,56 +9,40 @@
  * - $id: An optional CSS id to use for the layout.
  * - $content: An array of content, each item in the array is keyed to one
  *   panel of the layout. This layout supports the following sections:
- *   $content['sidebar']
- *   $content['content_top']
- *   $content['content_left']
- *   $content['content_right']
- *   $content['content_bottom']
- *   $content['sub_footer_left']
- *   $content['sub_footer_mid']
- *   $content['sub_footer_right']
- *   $content['footer']
+ *   $content['top_1']
+ *   $content['top_2']
+ *   $content['top_3']
+ *   $content['top_4']
+ *   $content['bottom_1']
+ *   $content['bottom_2']
+ *   $content['bottom_3']
  */
 ?>
 
-<section class="wwm-user-layout-special <?php if (!($renderer->prepared['regions']['sidebar']['pids'])) {print "no-sidebar ";}?>
-<?php if (!($renderer->prepared['regions']['content_left']['pids']) && !($renderer->prepared['regions']['content_right']['pids']) && !($renderer->prepared['regions']['content_top']['pids']) && !($renderer->prepared['regions']['content_bottom']['pids'])) {print "no-content ";}?>
-<?php if (!($renderer->prepared['regions']['content_left']['pids'])) {print "no-left ";}?>
-<?php if (!($renderer->prepared['regions']['content_right']['pids'])) {print "no-right ";}?>
-<?php if (!($renderer->prepared['regions']['sub_footer_left']['pids'])) {print "no-sub-footer-left ";}?>
-<?php if (!($renderer->prepared['regions']['sub_footer_mid']['pids'])) {print "no-sub-footer-mid ";}?>
-<?php if (!($renderer->prepared['regions']['sub_footer_right']['pids'])) {print "no-sub-footer-right ";}?>">
-  <section class="main">
-    <aside class="sidebar">
-      <?php print $content['sidebar']; ?>
-    </aside>
-    <section class="content-area">
-      <div class="content-top">
-        <?php print $content['content_top']; ?>
-      </div>
-      <div class="content-left">
-        <?php print $content['content_left']; ?>
-      </div>
-      <div class="content-right">
-        <?php print $content['content_right']; ?>
-      </div>
-      <div class="content-bottom">
-        <?php print $content['content_bottom']; ?>
-      </div>
+<main id="wwm-user-layout-special">
+  <section class="top-columns">
+    <section class="top-1 <?php if (!($renderer->prepared['regions']['top_1']['pids'])) {print "empty";}?>">
+      <?php print $content['top_1']; ?>
+    </section>
+    <section class="top-2 <?php if (!($renderer->prepared['regions']['top_2']['pids'])) {print "empty";}?>">
+      <?php print $content['top_2']; ?>
+    </section>
+    <section class="top-3 <?php if (!($renderer->prepared['regions']['top_3']['pids'])) {print "empty";}?>">
+      <?php print $content['top_3']; ?>
+    </section>
+    <section class="top-4 <?php if (!($renderer->prepared['regions']['top_4']['pids'])) {print "empty";}?>">
+      <?php print $content['top_4']; ?>
     </section>
   </section>
-  <footer class="sub-footer">
-    <div class="sub-footer-left">
-      <?php print $content['sub_footer_left']; ?>
-    </div>
-    <div class="sub-footer-mid">
-      <?php print $content['sub_footer_mid']; ?>
-    </div>
-    <div class="sub-footer-right">
-      <?php print $content['sub_footer_right']; ?>
-    </div>
-  </footer>
-  <footer class="footer">
-    <?php print $content['footer']; ?>
-  </footer>
-</section>
+  <section class="bottom-columns">
+    <section class="bottom-1 <?php if (!($renderer->prepared['regions']['bottom_1']['pids'])) {print "empty";}?>">
+      <?php print $content['bottom_1']; ?>
+    </section>
+    <section class="bottom-2 <?php if (!($renderer->prepared['regions']['bottom_2']['pids'])) {print "empty";}?>">
+      <?php print $content['bottom_2']; ?>
+    </section>
+    <section class="bottom-3 <?php if (!($renderer->prepared['regions']['bottom_3']['pids'])) {print "empty";}?>">
+      <?php print $content['bottom_3']; ?>
+    </section>
+  </section>
+</main>
